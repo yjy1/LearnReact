43783c05eb14759bab06ec4cdc832b4c5301a10b
yjy1 <318277446@qq.com>
2023-01-12 22:39:09 +0800

    tag_vscode中react插件的安装

---
+++ src/components/Welcome/demo.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class demo extends Component {
+  render() {
+    return (
+      <div>demo</div>
+    )
+  }
+}
+import second from 'first'
