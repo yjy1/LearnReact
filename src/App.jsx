3e74bd62a57efd40364ab8d73a897f5dcda083fb
yjy1 <318277446@qq.com>
2023-01-13 23:32:17 +0800

    tag_脚手架配置代理-方法1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
- import React, { Component } from 'react'
- 
- export default class App extends Component {
-   render() {
-     return (
-       <div>App</div>
-     )
-   }
- }
- 
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class App extends Component {
+  getStuData = () => {
+    axios.get('http://localhost:3000/index').then(
+      // axios.get('http://123.207.32.32:8000/home/multidata').then(
+      (response) => { console.log('成功了',response.data);},
+      (err) => {console.log('失败了',err); }
+    )
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.getStuData}>获取学生数据</button>
+      </div>
+    )
+  }
+}
