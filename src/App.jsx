f14b680a00882bdfcafb7f4510acceb8770c795e
yjy1 <318277446@qq.com>
2023-01-16 00:30:28 +0800

    tag_路由的基本使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
-import Search from './components/Search'
-import List from './components/List'
+import { Link, Route, Router } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Search />
-        <List />
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header"><h2>React Router Demo</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* 原生html中靠a标签跳转到不同的页面 */}
+              {/* <a className="list-group-item" href="./about.html">About</a>
+              <a className="list-group-item active" href="./home.html">Home</a> */}
+
+              {/* 在react中靠路由链接实现切换组件 */}
+          
+                <Link className="list-group-item" to={'/about'}> About</Link>
+                <Link className="list-group-item" to={'/home'}> Home </Link>
+            
+
+            </div>
+          </div>
+          <div className="col-xs-6">
+            {/* 注册路由 */}
+          
+              <Route path={'/about'} component={About}/>  
+              <Route path={'/home'} component={Home}/>  
+          
+          </div>
+        </div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 // 引入App组件
 import App from './App'
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root')
+)
