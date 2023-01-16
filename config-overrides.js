3f398307cda268d82cfc1842e313942afcb3dbb8
yjy1 <318277446@qq.com>
2023-01-16 22:20:50 +0800

    tag_antd自定义主题

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,15 @@
-  const { override, fixBabelImports } = require('customize-cra');
-  module.exports = override(
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+module.exports = override(
     fixBabelImports('import', {
-      libraryName: 'antd',
-      libraryDirectory: 'es',
-      style: 'css',
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
     }),
-  );
+    addLessLoader({
+        // 
+        lessOptions: { 
+            javascriptEnabled: true,
+            '@primary-color': '#1DA57A' 
+        },
+    }),
+);
