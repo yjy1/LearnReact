8ee9c6a5d79f9d17195e5dc554fe78de30468ce2
yjy1 <318277446@qq.com>
2023-01-17 14:45:31 +0800

    tag_求和案例_redux精简版

---
+++ Redux状态管理/redux_test/src/redux/count_reducer.js
@@ -0,0 +1,27 @@
+/* 
+    1.该文件时用于创建一个为Count组件服务的reducer,reducer的本质就是一个函数
+    2.reducer函数会接收到两个参数，分别为：之前的状态（preState）,动作对象（action）
+*/
+const initState = 0
+export default function countReducer(preState = initState, action) {
+    // if(preState === undefined) preState = 0
+    /* 
+        从action对象中获取：type、data
+    */
+ 
+    const { type, data } = action
+    // 根据type决定如何加工数据
+    switch (type) {
+        case 'increment':
+            console.log('countReducer increment',preState + parseInt(data) );
+            return preState + parseInt(data)   
+        case 'decrement':
+            return preState - parseInt(data)
+        case 'increment4Odd':
+            return preState + parseInt(data)
+        case 'incrementAsync':
+            return preState + parseInt(data)
+        default:
+            return preState
+    }
+}
