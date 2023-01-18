17ab7f39eb40e23d3298613252c476aef2b7ec1c
yjy1 <318277446@qq.com>
2023-01-18 16:57:36 +0800

    tag_扩展5_RefHook

--- React_extension扩展内容/react_extension/src/components/3_hooks/index.jsx
+++ React_extension扩展内容/react_extension/src/components/3_hooks/index.jsx
@@ -2,33 +2,39 @@ import React, { Component, hook } from 'react'
 import ReactDOM from 'react-dom'
 
 /* 类式组件 */
-/* 
-export default class Demo extends Component {
-  state = { count: 0 }
-  add = ()=>{
-      this.setState(state => ({ count: state.count + 1 }))
-  }
-  componentDidMount(){
-   this.timer = setInterval(() => {
-       this.setState(state => ({ count: state.count + 1 }))
-   }, 500);
-  }
-  unmount=()=>{
-     ReactDOM.unmountComponentAtNode(document.getElementById('root'))  
-  }
-  componentWillUnmount(){
-   clearInterval(this.timer)
-  }
-  render() {
-      return (
-          <div>
-              <h2>当前求和为：{this.state.count}</h2>
-              <button onClick={this.add}>点我加一</button>
-              <button onClick={this.unmount}>卸载组件</button>
-          </div>
-      )
-  }
-} */
+// export default class Demo extends Component {
+//     state = { count: 0 }
+//     myRef = React.createRef()
+    
+//     add = () => {
+//         this.setState(state => ({ count: state.count + 1 }))
+//     }
+//     show = () => {
+//         alert(this.myRef.current.value )
+//     }
+//     componentDidMount() {
+//         this.timer = setInterval(() => {
+//             this.setState(state => ({ count: state.count + 1 }))
+//         }, 500);
+//     }
+//     unmount = () => {
+//         ReactDOM.unmountComponentAtNode(document.getElementById('root'))
+//     }
+//     componentWillUnmount() {
+//         clearInterval(this.timer)
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <input type="text" ref={this.myRef} />
+//                 <h2>当前求和为：{this.state.count}</h2>
+//                 <button onClick={this.add}>点我加一</button>
+//                 <button onClick={this.unmount}>卸载组件</button>
+//                 <button onClick={this.show}>点击提示数据</button>
+//             </div>
+//         )
+//     }
+// }
 
 
 
@@ -37,26 +43,22 @@ export default function Demo() {
     console.log('start...');
     const [count, setCount] = React.useState(0) /* 数组的解构赋值 */
     const [name, setName] = React.useState('yangchengjie')
-    // let timer=null;
-    // const [{ count, name }, { setCount, setName }] = React.useState({ count: 0, name: 'yangchengjie' }) /* 不能写多个更新方法 */
-
+    const myRef = React.useRef()
+    console.log(myRef); 
+    
     // 加的回调
     function add() {
-        // setCount(count + 1) 第一种写法    
-        /*  type SetStateAction<S> = ((prevState: S) => S); */
         setCount(count => count + 1)
     }
     function changeName() {
         setName(name => 'tom' + count)
     }
+    function show(){
+        alert(myRef.current.value)
+    }
     function unmount() {
         ReactDOM.unmountComponentAtNode(document.getElementById('root'))
     }
-    /* 相当于componentDidUpdate; 第二个参数：数组，传入要监听的值 */
-    // React.useEffect(( )=>{
-    //     console.log('useEffect');
-    // },[count,name]) 
-
     /* 相当于componentDidMount */
     React.useEffect(() => {
         const timer = setInterval(() => {
@@ -67,17 +69,13 @@ export default function Demo() {
             clearInterval(timer)
         }
     }, [])
-    /* 相当于componentWillUnmount */
-    // React.useEffect(( ) => {
-    //     return ( ) => {
-    //         clearInterval( timer)
-    //     }
-    // }, [])
     return (
         <div>
             <h2>当前求和为：{count}</h2> <button onClick={add}>点我加一</button>
             <h2>当前姓名为：{name}</h2> <button onClick={changeName}>点我换姓名</button>
             <button onClick={unmount}>卸载组件</button>
+            <input type="text"  ref={myRef}/>
+            <button onClick={show}>点击提示数据</button>
         </div>
     )
 }
